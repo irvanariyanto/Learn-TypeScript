fb471c8002962725a81d7316e04d16266402422a
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2021-04-24 19:56:05 +0700

    Tipe data enum

---
+++ tipe-data-enum/tipedata-enum.ts
@@ -0,0 +1,11 @@
+enum Gender { Male = 1, Female = 2, Unknown = -1 };
+
+let jenisKelamin = Gender.Male;
+
+console.log(jenisKelamin);
+
+enum Category { Handphone = "HANDPHONE", Komputer = "KOMPUTER"}
+
+let productCategory = Category.Handphone;
+
+console.log(productCategory);
