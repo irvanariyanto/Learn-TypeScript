8233ddd0ab45aa23aa614db2713bb926e9e011ba
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2021-04-25 04:21:32 +0700

    Inheritance

--- build/class.js
+++ build/class.js
@@ -1,13 +1,51 @@
 "use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.User = void 0;
 var User = /** @class */ (function () {
     function User(name, age) {
+        var _this = this;
         this.age = age;
+        this.getName = function () {
+            return _this.name;
+        };
         this.name = name;
     }
+    User.prototype.setName = function (value) {
+        this.name = this.name;
+    };
     return User;
 }());
 exports.User = User;
-var user = new User("Irvan", 10);
-console.log(user);
+var Admin = /** @class */ (function (_super) {
+    __extends(Admin, _super);
+    function Admin() {
+        var _this = _super !== null && _super.apply(this, arguments) || this;
+        _this.read = true;
+        _this.write = true;
+        return _this;
+    }
+    Admin.prototype.getRole = function () {
+        return {
+            read: this.read,
+            write: this.write
+        };
+    };
+    return Admin;
+}(User));
+var admin = new Admin("Irvan", 24);
+console.log(admin.getName() + " " + admin.getRole().write);
--- src/class.ts
+++ src/class.ts
@@ -4,8 +4,29 @@ export class User {
     constructor(name: string, public age: number){
         this.name = name;
     }
+
+    setName(value: string): void{
+        this.name = this.name;
+    }
+
+    getName = (): string => {
+        return this.name;
+    }
 }
 
-let user = new User("Irvan", 10);
 
-console.log(user);
+class Admin extends  User{
+    read: boolean = true;
+    write: boolean = true;
+
+    getRole(): {read: boolean, write: boolean}{
+        return{
+            read: this.read,
+            write: this.write
+        }
+    }
+}
+
+let admin = new Admin("Irvan", 24);
+
+console.log(`${admin.getName()} ${admin.getRole().write}`)
