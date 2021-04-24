1dce2c684f56164c253ea121adaae0876610b954
Irvan Ariyanto <irvan.ariyanto104@gmail.com>
2021-04-25 03:59:22 +0700

    Tipe data object + Union type

---
+++ src/object.ts
@@ -0,0 +1,15 @@
+// Object
+
+type User = {name: string, age: number};
+  
+let user: User = {
+    name: "Irvan",
+    age: 15
+}
+
+// Union type
+
+let phone: number | string ;
+
+phone = 62819898;
+phone = "6236516526";
